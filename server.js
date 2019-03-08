8eb16295fab3bd34ab0494a7804ba4187bf92ec0
zeinab <zeinabsamir8@gmail.com>
2019-03-08 18:00:34 +0200

    connect to pg

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+
+const db = require('./queries')
 app.set('port', (process.env.PORT || 3000));
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/users', db.getUsers);
 
 app.listen(app.get('port'), () => console.log(`App listening on port ${app.get('port')}`));
