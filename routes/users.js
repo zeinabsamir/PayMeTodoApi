ff31aac460720cca1dbbad5c038dc7fe20127c40
zeinab <zeinabsamir8@gmail.com>
2019-03-09 18:03:06 +0200

    Fix an error

--- routes/users.js
+++ routes/users.js
@@ -11,20 +11,20 @@ const pool = new Pool({
 });
 
 function verfiyToken(req, res, next) {
-  if (!req.headers.authorization) {
-    return res.status(401).send("Unauthorized Request");
+    if (!req.headers.authorization) {
+      return res.status(401).send("Unauthorized Request");
+    }
+    let token = req.headers.authorization.split(" ")[1];
+    if (token === "null") {
+      return res.status(401).send("Unauthorized Request");
+    }
+    jwt.verify(token, "secret", function(err, decoded) {
+      if (err) return res.status(401).send("Unauthorized Request");
+  
+      req.accountId = decoded.accountId;
+      next();
+    });
   }
-  let token = req.headers.authorization.split(" ")[1];
-  if (token === "null") {
-    return res.status(401).send("Unauthorized Request");
-  }
-  jwt.verify(token, "secret", function(err, decoded) {
-    if (err) return res.status(401).send("Unauthorized Request");
-
-    req.accountId = decode.accountId;
-    next();
-  });
-}
 
 router.get("/", (req, res) => {
   pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
@@ -45,10 +45,9 @@ router.post("/login", (req, res) => {
       if (error) {
         throw error;
       }
-      if (results.rows[0].username !== username) {
-        res.status(401).send("Invalid username");
-      } else if (results.rows[0].password !== password) {
-        res.status(401).send("Invalid password");
+      console.log(results.rows)
+      if (!results.rows[0]) {
+        res.status(401).send("Invalid username or password");
       } else {
         // console.log(results.rows[0].id);
         let id = results.rows[0].id;
@@ -61,6 +60,7 @@ router.post("/login", (req, res) => {
 });
 
 router.get("/listTodos", verfiyToken, (req, res) => {
+
   const userId = req.accountId;
   pool.query(
     "SELECT * FROM todos WHERE user_id = $1 ",
@@ -70,13 +70,13 @@ router.get("/listTodos", verfiyToken, (req, res) => {
         throw error;
       }
       console.log(results);
-      // response.status(201).send(`Todo added with ID: ${result.insertId}`)
+      res.status(200).send(results.rows)
     }
   );
 });
 
 router.post("/addTodo", verfiyToken, (req, res) => {
-  const content = request.body;
+  const { content } = req.body;
   const userId = req.accountId;
   pool.query(
     "INSERT INTO todos (content, user_id) VALUES ($1, $2)",
