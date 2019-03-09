d88eecaca72d76444ffea189f28142814915e856
zeinab <zeinabsamir8@gmail.com>
2019-03-09 07:03:04 +0200

    Add routes for create, list and delete todos for specific user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const jwt =require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 const Pool = require("pg").Pool;
 
@@ -10,66 +10,99 @@ const pool = new Pool({
   ssl: true
 });
 
-router.get('/',(req,res) =>{
-    pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
-        if (error) {
-          throw error;
-        }
-        res.status(200).json(results.rows);
-      });
-   
-})
+function verfiyToken(req, res, next) {
+  if (!req.headers.authorization) {
+    return res.status(401).send("Unauthorized Request");
+  }
+  let token = req.headers.authorization.split(" ")[1];
+  if (token === "null") {
+    return res.status(401).send("Unauthorized Request");
+  }
+  jwt.verify(token, "s3cr3t", function(err, decoded) {
+    if (err) return res.status(401).send("Unauthorized Request");
 
-router.post("/login",(req,res)=>{
-   // console.log(req.body);
-    const {username, password } = req.body;
-    pool.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password], (error, results) => {
-        if (error) {
-          throw error
-        }
-        if(results.rows[0].username !== username) {
-            res.status(401).send('Invalid username')
-        } else if(results.rows[0].password !== password) {
-            res.status(401).send('Invalid password');
-        }
-        else {
-           // console.log(results.rows[0].id);
-            let payload = {accountId: results.rows[0].id};
-            let token=jwt.sign(payload,"secret");
-            res.status(200).json({token});
-         }
-       
-      })
+    req.accountId = decode.accountId;
+    next();
+  });
+}
 
-//    loginSchema.findOne({"email":req.body.email,"password":req.body.password}, function (err,user) {
-//      console.log(user);
-//      if (err) res.send({"error":"uesr name or password is in correct"});
-//      if(user!=null){
-//        let payload={subject:{'accountId':user.accountId,"userType":user.userType}}
-//        let token=jwt.sign(payload,"secret")
-//      if(user.userType==false)
-//      {
-//      candidateSchema.find({accountId:user.accountId},(error,result)=>{
-//        if (error) res.send({"error":"uesr name or password is in correct"}); 
-//        else
-//        { 
-//          res.send({token,userType:user.userType,accountId:user.accountId});
-//        }
-      
-//      })
-//    }
-//      else if(user.userType==true)
-//      {
-//      empSchema.find({accountId:user.accountId},(error,result)=>{
-//        if (error) res.send({"error":error}); 
-//        else
-//        {
-//          res.send({token,userType:user.userType,accountId:user.accountId});
-//        } 
-//      })
-//    }
-//  }
-//    });
-  })
+router.get("/", (req, res) => {
+  pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+});
 
+router.post("/login", (req, res) => {
+  // console.log(req.body);
+  const { username, password } = req.body;
+  pool.query(
+    "SELECT * FROM users WHERE username = $1 AND password = $2",
+    [username, password],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      if (results.rows[0].username !== username) {
+        res.status(401).send("Invalid username");
+      } else if (results.rows[0].password !== password) {
+        res.status(401).send("Invalid password");
+      } else {
+        // console.log(results.rows[0].id);
+        let id = results.rows[0].id;
+        let payload = { accountId: results.rows[0].id };
+        let token = jwt.sign(payload, "secret");
+        res.status(200).json({ token, id});
+      }
+    }
+  );
+});
+
+router.get("/listTodos", verfiyToken, (req, res) => {
+  const userId = req.accountId;
+  pool.query(
+    "SELECT * FROM todos WHERE user_id = $1 ",
+    [userId],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      console.log(results);
+      // response.status(201).send(`Todo added with ID: ${result.insertId}`)
+    }
+  );
+});
+
+router.post("/addTodo", verfiyToken, (req, res) => {
+  const content = request.body;
+  const userId = req.accountId;
+  pool.query(
+    "INSERT INTO todos (content, user_id) VALUES ($1, $2)",
+    [content, userId],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      console.log(results);
+      // response.status(201).send(`Todo added with ID: ${result.insertId}`)
+    }
+  );
+});
+router.get("/deleteTodo/:id", verfiyToken, (req, res) => {
+  const todoId = req.params.id;
+  const userId = req.accountId;
+  pool.query(
+    "DELETE FROM todos WHERE  id = $1 AND user_id = $2",
+    [todoId, userId],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      console.log(results);
+      // response.status(201).send(`Todo deleted with ID: ${result.insertId}`)
+    }
+  );
+});
 module.exports = router;
